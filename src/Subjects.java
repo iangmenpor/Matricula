90bcc2d8383e9fcefc260a4cebfa13fcea614a85
iangmenpor <145766951+iangmenpor@users.noreply.github.com>
2023-10-26 17:23:02 +0200

    Ejercicio Matricula

---
+++ src/Subjects.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Subjects {
+
+    private String code;
+    private String name;
+
+    private ArrayList<Subjects> subjects;
+
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/Tuition.java
@@ -0,0 +1,31 @@
+public class Tuition {
+
+    private String code; //click derech./generate../getter and setter
+    private String startyear;
+
+    private String endyear;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getStartyear() {
+        return startyear;
+    }
+
+    public void setStartyear(String startyear) {
+        this.startyear = startyear;
+    }
+
+    public String getEndyear() {
+        return endyear;
+    }
+
+    public void setEndyear(String endyear) {
+        this.endyear = endyear;
+    }
+}
